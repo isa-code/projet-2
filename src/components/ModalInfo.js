96084edce0791e3a68738a1fdfe4855e4807d757
isabelle Guilherme <isabelle.guilherme@csenen.com>
2021-03-12 16:34:27 +0100

    favorite et supression

--- src/components/ModalInfo.js
+++ src/components/ModalInfo.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
-import Dialog from '@material-ui/core/Dialog';
-import { useState, useEffect } from 'react';
-import '../components/StyleGeneral.css'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import Dialog from "@material-ui/core/Dialog";
+import { useState, useEffect } from "react";
+import "../components/StyleGeneral.css";
 
 function ModalInfo(props) {
-  
   const [infoFilm, SetInfoFilm] = useState(0);
   const { onClose, selectedValue, open } = props;
   const casting = props.filmCasting;
-  const [watchProviders, SetWatchProviders] = useState(null)
+  const [watchProviders, SetWatchProviders] = useState(null);
+  // const [newCasting, setCasting] = useState(0);
 
   const handleClose = () => {
     onClose(selectedValue);
@@ -20,45 +20,62 @@ function ModalInfo(props) {
     onClose(value);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     SetInfoFilm(props.infoFilm);
-    /*fetch('https://api.themoviedb.org/3/movie/'+ infoFilm.id +'/credits?api_key=0eb1560cadbbc71b973ed8f868ef57fa&language=fr-FR')
-            .then((resp) => resp.json())
-            .then((data) => { setCasting(data)})*/
-  },[]);
-
+    // fetch(
+    //   "https://api.themoviedb.org/3/movie/" +
+    //     infoFilm.id +
+    //     "/credits?api_key=0eb1560cadbbc71b973ed8f868ef57fa&language=fr-FR"
+    // )
+    //   .then((resp) => resp.json())
+    //   .then((data) => {
+    //     setCasting(data);
+    //   });
+  }, []);
 
   return (
-    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-      <img src={'https://image.tmdb.org/t/p/original/'+infoFilm.backdrop_path}/>
-      
-      <div className='contentModal'>
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="simple-dialog-title"
+      open={open}
+    >
+      <img
+        src={"https://image.tmdb.org/t/p/original/" + infoFilm.backdrop_path}
+        alt="affiche du film"
+      />
+
+      <div className="contentModal">
         <div>
-          <Button 
+          <Button
             variant="contained"
             className="redBtn allBtn"
-            startIcon={<PlayCircleFilledIcon />}>
-              Regarder</Button>
+            startIcon={<PlayCircleFilledIcon />}
+          >
+            Regarder
+          </Button>
           <h3>{infoFilm.title}</h3>
         </div>
-        <div className='blockRanking'>
-          {props.ranking} 
+        <div className="blockRanking">
+          {props.ranking}
           <p>{props.rankingNote}/10</p>
         </div>
         <p>{infoFilm.overview}</p>
-        <p><b>Casting :</b></p>
-        <div className='casting'>
-          {
-          casting !== null &&
-          casting.cast.map( actor => (
-            <div key={actor.id}>
-              <img src={'https://image.tmdb.org/t/p/w200/'+actor.profile_path} alt={'Portrait de '+ actor.name}/>
-              <p>{actor.name}</p>
-            </div>
-          ))
-          }
+        <p>
+          <b>Casting :</b>
+        </p>
+        <div className="casting">
+          {casting !== null &&
+            casting.cast.slice(0, 5).map((actor) => (
+              <div key={actor.id}>
+                <img
+                  src={"https://image.tmdb.org/t/p/w200/" + actor.profile_path}
+                  alt={"Portrait de " + actor.name}
+                />
+                <p>{actor.name}</p>
+              </div>
+            ))}
         </div>
-      </div>  
+      </div>
     </Dialog>
   );
 }
--- src/components/cardFilm.js
+++ src/components/cardFilm.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import '../components/StyleGeneral.css';
-import InfoBtn from '../components/InfoBtn';
-import './RandomCards.css';
+import React from "react";
+import "../components/StyleGeneral.css";
+import InfoBtn from "../components/InfoBtn";
+import "./RandomCards.css";
 
 export default function CardFilm(props) {
+  const titleFilm = props.title;
+  const rankingFilm = props.ranking;
+  const rankingNoteFilm = props.rankingNote;
 
+  return (
+    <div className="cardFilm">
+      <img src={props.poster} alt={titleFilm} />
+      <div className="blocktexte">
+        <div>
+          <p>{titleFilm}</p>
 
-
-    const titleFilm = props.title;
-    const rankingFilm = props.ranking;
-    const rankingNoteFilm = props.rankingNote;
-
-    return (
-        <div className="cardFilm">
-          <img src={props.poster} alt={titleFilm}/>
-          <div className='blocktexte'>
-                <div>
-                    <div className='blockRanking'>
-                      {rankingFilm} 
-                      <p>{rankingNoteFilm}/10</p>
-                    </div>
-                    <p>{props.year}</p>
-                    <p><small>{props.genres}</small></p>
-                </div>
-                <InfoBtn infoFilm={props.infoFilm} ranking={rankingFilm}
-                        rankingNote={rankingNoteFilm} filmCasting={props.filmCasting}/> 
+          <div className="blockRanking">
+            {rankingFilm}
+            <p>{rankingNoteFilm}/10</p>
           </div>
-          
+
+          <p>{props.year}</p>
+
+          <p>
+            <small>{props.genres}</small>
+          </p>
         </div>
-    );
+        <InfoBtn
+          infoFilm={props.infoFilm}
+          ranking={rankingFilm}
+          rankingNote={rankingNoteFilm}
+          filmCasting={props.filmCasting}
+        />
+      </div>
+    </div>
+  );
 }
